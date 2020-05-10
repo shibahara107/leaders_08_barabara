e9f7dcfb4bc1f2920eaac082149123612b4aba6e
Yoshinori Shibahara <yoshinori.shibahara@gmail.com>
2020-05-10 13:22:23 +0900

    First commit

---
+++ leaders_08_barabaraTests/leaders_08_barabaraTests.swift
@@ -0,0 +1,34 @@
+//
+//  leaders_08_barabaraTests.swift
+//  leaders_08_barabaraTests
+//
+//  Created by Yoshinori Shibahara on 2020/05/09.
+//  Copyright © 2020 Yoshinori Shibahara. All rights reserved.
+//
+
+import XCTest
+@testable import leaders_08_barabara
+
+class leaders_08_barabaraTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
